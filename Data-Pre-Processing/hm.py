a5515a49cc772e5b355ee5ef2f1490db3746402c
rrcasaque <rrcasaque@hotmail.com>
2021-08-24 17:07:43 -0300

    first upload

---
+++ Data-Pre-Processing/hm.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandas.core.dtypes.missing import isnull
+from pandas.core.indexes import base
+import plotly.express as px 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb 
+
+base_credito = pd.read_csv('Data-Pre-Processing\credit_data.csv') #realiza a importação do banco de dados
+base_credito.loc[base_credito['person_age']>99, 'person_age'] = 27.71804 #atribui a media às idades>99
+base_credito['loan_int_rate'].fillna(base_credito['loan_int_rate'].mean(), inplace=True) #substitui os valores vazios de loan_rate_int pela média 
+base_credito['person_emp_length'].fillna(base_credito['person_emp_length'].mean(), inplace=True) #substitui os valores vazios de person_emp_length pela média 
+x_credito = base_credito.iloc[:,0:10].values #atribui À variável x_credito apenas as colunas de 0-9 (:) indica todas as linhas
+y_credito = base_credito.iloc[:,10].values #atribui À variável y_credito apenas a colunas 11
+
+print(x_credito[:,0]) #exibe todas as linhas apenas da coluna 0 'person_age'
+print(x_credito[:,0].min()) #exibe o menor valor
+print(x_credito[:,0].max()) #exibe o maior valor
+
+# É importante deixar todos os valores na mesma escala, senão o algorítmo pode dar mais relevância a determinado atributo em detrimento de outro
+
+# Formulas de fazer essa operação:
+
+# Padronização (Standardisation) ===> x = x - média(x) / desvio padrão(x)
+# Normalização (Normalization) ===> x = x - mínimo(x) / máximo(x) - mínimo(x)
+
+# Desvio padrão == quanto os valores variam quando comparados à média
+
+# A Padronização é mais indicada quando há registros muito fora do padrão (outliers)
