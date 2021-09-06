22d6b0f01450c13137237f50059df082a3c1b083
rafael rocha <rrcasaque@hotmail.com>
2021-09-06 14:18:16 -0300

    first upload

--- Bayesian-Learning/Naïve Bayes - credit base.py
+++ Bayesian-Learning/Naïve Bayes - credit base.py
@@ -1,10 +1,11 @@
 import pickle
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from yellowbrick.classifier import ConfusionMatrix
+import matplotlib.pyplot as plt
 
 
-with open('Bayesian-Learning\credito.pkl', 'rb') as f: #carrega o arquivo pré-processado anteriormente
+with open('Data-Science-and-Machine-Learning\Bayesian-Learning\credito.pkl', 'rb') as f: #carrega o arquivo pré-processado anteriormente
     x_credito_treinamento, y_credito_treinamento, x_credito_teste, y_credito_teste = pickle.load(f) #armazena as informações do arquivo nas variáveis indicadas
 
 print(x_credito_treinamento.shape)
@@ -29,4 +30,17 @@ print(confusion_matrix(y_credito_teste, previsoes)) #retorna dois arrays, indica
 #[clientes que não pagam e foram classificados como pagantes, clientes que não pagam e foram classificados como tal]
 
 cm = ConfusionMatrix(naive_dados_credito)
-cm.fit()
+cm.fit(x_credito_treinamento, y_credito_treinamento)
+cm.score(x_credito_teste, y_credito_teste)
+# print(cm.score(x_credito_teste, y_credito_teste)) também retorna a taxa de acerto
+
+plt.show()  # o gráfico exibido exemplifica as classificações do seguinte modo:
+# coordenada (0,0) clientes que pagam e foram classificados como tal
+# coordenada (0,1) clientes que não pagam e foram classificados como pagantes
+# coordenada (1,1) clientes que não pagam e foram classificados como tal
+# coordenada (1,0) clientes que pagam e foram classificados como não pagantes
+
+print(classification_report(y_credito_teste, previsoes)) # exibe os status de desempenho do algorítmo 
+
+# recall significa o quanto ele consegue indificar corretamente dos registros 
+# e após identificar, o precision indica a precisão de acertos
